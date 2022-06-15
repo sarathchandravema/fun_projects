47e263fe24627c899d1491aea9c35869df0a6010
sarathchandravema <rama.sarath.c.vema@accenture.com>
2022-06-15 18:02:16 +0530

    fun project 1, updated

--- basic_madlib.py
+++ basic_madlib.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 def madlib():
     adj = input("Adjective:  ")
     verb1 = input("Verb:  ")
     verb2 = input("Verb:  ")
     famous_person = input("Famous person:  ")
     
-    madlibs = f"\nComputer Programming is so {adj}! It makes me so excited all the time because I love to {verb1}.     Stay hyderated and {verb2} like you are {famous_person}!"
+    madlibs = f"\nComputer Programming is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hyderated and {verb2} like you are {famous_person}!"
     
     return madlibs
-
-
-# In[ ]:
-
 
 if __name__ == "__main__":
     output = madlib()
     print(output)
-
